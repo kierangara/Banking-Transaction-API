f13ae4a9c0bf770d6f0a617e2fc35be4ee189220
Kieran Gara <kierangara@gmail.com>
2025-02-05 12:09:57 -0500

    Fixed missing field reason message

--- src/main/java/com/banking/transactions/exceptions/MissingInformationException.java
+++ src/main/java/com/banking/transactions/exceptions/MissingInformationException.java
@@ -2,6 +2,6 @@ package com.banking.transactions.exceptions;
 
 public class MissingInformationException extends RuntimeException {
     public MissingInformationException(String missingField) {
-        super(missingField + " is a required field and must be greater than 1.");
+        super(missingField + " is a required field and must be greater than 0.");
     }
 }
